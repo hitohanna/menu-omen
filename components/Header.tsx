05b55e0d440c458cd7a1463259a6c61ac6fd590c
Hitohanna <htoes.ndweeds@gmail.com>
2023-11-09 02:02:27 +0700

    add header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,33 @@
 export const Header: React.FC = () => {
-  return <header></header>;
+  return (
+    <head>
+      <title>Rombong Omen</title>
+      <meta
+        name="Menu Online Rombong Omen"
+        content="Menu online untuk kamu biar gak bingung lagi kalo pesen di Rombong Omen, yuk di pesan"
+      />
+
+      <meta property="og:url" content="https://menu-omen.vercel.app/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Menu Online Rombong Omen" />
+      <meta
+        property="og:description"
+        content="Menu online untuk kamu biar gak bingung lagi kalo pesen di Rombong Omen, yuk di pesan"
+      />
+      <meta property="og:image" content="https://menu-omen.vercel.app/og.png" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="menu-omen.vercel.app" />
+      <meta property="twitter:url" content="https://menu-omen.vercel.app/" />
+      <meta name="twitter:title" content="Menu Online Rombong Omen" />
+      <meta
+        name="twitter:description"
+        content="Menu online untuk kamu biar gak bingung lagi kalo pesen di Rombong Omen, yuk di pesan"
+      />
+      <meta
+        name="twitter:image"
+        content="https://menu-omen.vercel.app/og.png"
+      />
+    </head>
+  );
 };
